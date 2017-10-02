1a96cd0370f84a4476606d22bc02154a4a845921
Rusty Ballinger <rusty.ballinger@gmail.com>
2017-10-02 02:19:25 -0700

    Fiddling with GameOverActivity layout

--- app/src/main/java/com/scottrealapps/calculater/GameOverActivity.java
+++ app/src/main/java/com/scottrealapps/calculater/GameOverActivity.java
@@ -5,10 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
-
-import com.scottrealapps.calculater.R;
-import com.scottrealapps.calculater.StartGameActivity;
 
 public class GameOverActivity extends AppCompatActivity {
 
@@ -32,12 +31,28 @@ public class GameOverActivity extends AppCompatActivity {
 
         TextView scoreText = (TextView)findViewById(R.id.scoreText);
         TextView message = (TextView)findViewById(R.id.message);
-        TextView highScores = (TextView)findViewById(R.id.highScores);
+        TableLayout highScores = (TableLayout) findViewById(R.id.highScores);
 
-scoreText.setText(score + ", speed: " + speed);
+scoreText.setText(score + ", speed " + speed);
 message.setText("You are great!");
-highScores.setText("(High scores would go here)");
+//some bogus high scores
+setScoreStuff((TableRow)(highScores.getChildAt(1)), "EPB", 666, 123, "2017-10-3");
+setScoreStuff((TableRow)(highScores.getChildAt(2)), "EPB", 665, 123, "2017-10-2");
+setScoreStuff((TableRow)(highScores.getChildAt(3)), "RDB", 101,  21, "2017-10-1");
+setScoreStuff((TableRow)(highScores.getChildAt(4)), "EPB",  57,  12, "2017-10-1");
+setScoreStuff((TableRow)(highScores.getChildAt(5)), "RDB",   3,   6, "2017-10-1");
+    }
 
+    private void setScoreStuff(TableRow row, String name, int score, int speed, String date) {
+        if (row == null) return;
+        TextView tv = (TextView)(row.getChildAt(0));
+        tv.setText(name);
+        tv = (TextView)(row.getChildAt(1));
+        tv.setText(Integer.toString(score));
+        tv = (TextView)(row.getChildAt(2));
+        tv.setText(Integer.toString(speed));
+        tv = (TextView)(row.getChildAt(3));
+        tv.setText(date);
     }
 
     /**
